7398139b7b8b3d578327eeb94b9da459f99ea051
AbdelRadwan <21023979@student.ltuc.com>
2024-07-01 00:54:04 +0300

    Add project files.

---
+++ AdventureQuestRPG/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventureQuestRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Player player = new Player("Abood", 100, "Sword");
+                player.DesblayInfo();
+                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Dragon dragon = new Dragon("Dragon", 100, "Scales");
+                dragon.DesblayInfo();
+                Console.WriteLine("");
+
+                Falcon falcon = new Falcon("Falcon", 100, "Scales");
+
+                BattleSystem battleSystem = new BattleSystem();
+                // battleSystem.StartBattel(dragon, player);
+
+                battleSystem.StartBattel(falcon, player);
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.ReadKey();
+        }
+    }
+}
